fbb00bd67c2f5e84780dfa7da1f3bef21c82026d
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2024-12-13 15:50:46 +0800

    chore: remove schoolName in find all clinics by school dto

--- src/api/clinic/dto/get-all-clinic-by-school.dto.ts
+++ src/api/clinic/dto/get-all-clinic-by-school.dto.ts
@@ -2,7 +2,6 @@ import { z } from "zod";
 
 export const FindAllClinicBySchoolSchema = z.object({
   schoolId: z.string().min(1, { message: "should not be empty" }),
-  schoolName: z.string().min(1, { message: "should not be empty" }),
 });
 
 export type FindAllClinicBySchoolDto = z.infer<typeof FindAllClinicBySchoolSchema>;
