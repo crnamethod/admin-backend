5c82c706e58736a9acc6ce23193060f0fb8b2788
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2024-12-16 15:39:31 +0800

    refactor: change structure

--- src/api/user/userController.ts
+++ src/api/user/userController.ts
@@ -1,52 +1,47 @@
-import type { Request, RequestHandler, Response } from "express";
+import type { Request, Response } from "express";
 import expressAsyncHandler from "express-async-handler";
 import type { UploadedFile } from "express-fileupload";
 
 import { userService } from "@/api/user/userService";
 import type { TypedRequestBody } from "@/common/types/request.type";
-import { handleServiceResponse } from "@/common/utils/httpHandlers";
 
 import type { UpdatePasswordDto } from "./dto/update-password.dto";
+import type { UpdateProfileDto } from "./dto/update-profile.dto";
 import type { UserProfileBodyDto } from "./dto/upload-photo.dto";
 
 class UserController {
-  public getUsers: RequestHandler = async (_req: Request, res: Response) => {
+  public getUsers = expressAsyncHandler(async (req, res) => {
     const serviceResponse = await userService.findAll();
-    return handleServiceResponse(serviceResponse, res);
-  };
+    res.status(serviceResponse.statusCode).send(serviceResponse);
+  });
 
-  public getUser: RequestHandler = async (req: Request, res: Response) => {
+  public getUser = expressAsyncHandler(async (req: Request, res: Response) => {
     const serviceResponse = await userService.findOneOrThrow(req.params.id);
-    return handleServiceResponse(serviceResponse, res);
-  };
+    res.status(serviceResponse.statusCode).send(serviceResponse);
+  });
 
-  public updateUser: RequestHandler = async (req: Request, res: Response) => {
+  public updateUser = expressAsyncHandler(async (req: TypedRequestBody<UpdateProfileDto>, res: Response) => {
     const serviceResponse = await userService.update(req.params.userId, req.body);
-    return handleServiceResponse(serviceResponse, res);
-  };
+    res.status(serviceResponse.statusCode).send(serviceResponse);
+  });
 
-  public createUser: RequestHandler = async (req: Request, res: Response) => {
+  public createUser = expressAsyncHandler(async (req: Request, res: Response) => {
     const userProfile = req.body as { email: string; userId: string };
 
     const serviceResponse = await userService.create(userProfile);
-    return handleServiceResponse(serviceResponse, res);
-  };
+    res.status(serviceResponse.statusCode).send(serviceResponse);
+  });
 
-  public changePassword: RequestHandler = async (req: TypedRequestBody<UpdatePasswordDto>, res: Response) => {
+  public changePassword = expressAsyncHandler(async (req: TypedRequestBody<UpdatePasswordDto>, res: Response) => {
     await userService.changePassword(req.body.email, req.body.password);
     res.status(200).json({ message: "Password changed successfully " });
-  };
+  });
 
-  public uploadPicture: RequestHandler = expressAsyncHandler(
-    async (req: TypedRequestBody<UserProfileBodyDto>, res: Response) => {
-      const data = await userService.upload(req.body, req.files?.file as UploadedFile);
+  public uploadPicture = expressAsyncHandler(async (req: TypedRequestBody<UserProfileBodyDto>, res: Response) => {
+    const serviceResponse = await userService.upload(req.body, req.files?.file as UploadedFile);
 
-      res.status(200).json({
-        message: "Image uploaded successfully",
-        data,
-      });
-    },
-  );
+    res.status(serviceResponse.statusCode).json(serviceResponse);
+  });
 }
 
 export const userController = new UserController();
