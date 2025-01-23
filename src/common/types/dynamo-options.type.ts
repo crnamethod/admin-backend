84f891e1ea3c91ed0d607c909a0cd8e1b798c8b7
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2025-01-23 21:12:58 +0800

    feat: add query command options

--- src/common/types/dynamo-options.type.ts
+++ src/common/types/dynamo-options.type.ts
@@ -1,5 +1,6 @@
 import type { KeysAndAttributes } from "@aws-sdk/client-dynamodb";
-import type { GetCommandInput } from "@aws-sdk/lib-dynamodb";
+import type { GetCommandInput, QueryCommandInput } from "@aws-sdk/lib-dynamodb";
 
 export type BatchGetCommandOptions = Omit<KeysAndAttributes, "Keys">;
 export type GetCommandOptions = Omit<GetCommandInput, "TableName" | "Key">;
+export type QueryCommandOptions = Omit<QueryCommandInput, "TableName" | "IndexName">;
