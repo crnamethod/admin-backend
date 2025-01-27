2d821c335881bdcea87629311f89d0b6c3865687
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2025-01-27 13:47:54 +0800

    feat: add paginated clinic reviews; change find one clinic review

--- src/api/clinic-review/clinic-review.repository.ts
+++ src/api/clinic-review/clinic-review.repository.ts
@@ -1,12 +1,11 @@
 import {
   GetCommand,
   type GetCommandInput,
-  PutCommand,
-  type PutCommandInput,
   QueryCommand,
   type QueryCommandInput,
   UpdateCommand,
   type UpdateCommandInput,
+  paginateQuery,
 } from "@aws-sdk/lib-dynamodb";
 
 import type { GetCommandOptions } from "@/common/types/dynamo-options.type";
@@ -16,12 +15,125 @@ import { calculateAverage } from "@/common/utils/number";
 import { updateDataHelper } from "@/common/utils/update";
 
 import { clinicService } from "../clinic/clinic.service";
+import type { FindAllClinicReviewDto } from "./dto/get-all-clinic-review.dto";
 import type { UpdateClinicReviewDto } from "./dto/update-clinic-review.dto";
 import { ClinicReviewEntity } from "./entity/clinic-review.entity";
 
 const TableName = env.DYNAMODB_TBL_CLINIC_REVIEWS;
 
 class ClinicReviewRepository {
+  async findAll(query: FindAllClinicReviewDto) {
+    const { limit = 10, startingToken, ...filters } = query || {};
+
+    const params = this.findAllQuery({ limit, ...filters });
+
+    console.log("Query Command Params: ", JSON.stringify(params, null, 2));
+
+    const paginator = paginateQuery({ client: dynamoClient, startingToken }, params);
+
+    const accumulatedItems: any[] = [];
+    let lastEvaluatedKey: any;
+
+    for await (const page of paginator) {
+      const remainingLimit = limit - accumulatedItems.length;
+      accumulatedItems.push(...page.Items!.slice(0, remainingLimit));
+
+      // ? If the accumulated items length is exactly the same with the limit
+      if (remainingLimit % limit === 0 && accumulatedItems.length >= limit) {
+        lastEvaluatedKey = page.LastEvaluatedKey;
+        break;
+      } else if (accumulatedItems.length >= limit) {
+        break;
+      }
+
+      // ? Reassign the value for the last evaluated key if there's still remaining items to be pushed
+      lastEvaluatedKey = page.LastEvaluatedKey;
+    }
+
+    const reviews = accumulatedItems.map((item) => new ClinicReviewEntity(item));
+
+    return {
+      lastEvaluatedKey: lastEvaluatedKey ?? null,
+      total: reviews.length,
+      data: reviews,
+    };
+  }
+
+  private findAllQuery(filters: Omit<FindAllClinicReviewDto, "startingToken">) {
+    const { search, limit, sort_by_date, startDate, endDate, clinicId, userId, rating, status } = filters;
+
+    const params: QueryCommandInput = {
+      TableName,
+      Limit: limit,
+      ScanIndexForward: sort_by_date === "asc", // true for ascending, false for descending
+      ExpressionAttributeNames: {},
+      ExpressionAttributeValues: {},
+    };
+
+    const filterExpressions: string[] = [];
+
+    if (clinicId) {
+      params.IndexName = "ClinicCreatedAtIndex";
+      params.KeyConditionExpression = "clinicId = :clinicId";
+      params.ExpressionAttributeValues![":clinicId"] = clinicId;
+    } else if (userId) {
+      params.IndexName = "UserCreatedAtIndex";
+      params.KeyConditionExpression = "userId = :userId";
+      params.ExpressionAttributeValues![":userId"] = userId;
+    } else {
+      params.IndexName = "CreatedAtIndex";
+      params.KeyConditionExpression = "#gsiKey = :gsiValue";
+      params.ExpressionAttributeValues![":gsiValue"] = "ALL";
+      params.ExpressionAttributeNames!["#gsiKey"] = "gsiPartitionKey";
+    }
+
+    if (startDate && endDate) {
+      params.KeyConditionExpression += " AND createdAt BETWEEN :startDate AND :endDate";
+      params.ExpressionAttributeValues![":startDate"] = startDate;
+      params.ExpressionAttributeValues![":endDate"] = endDate;
+    }
+
+    const addFilterExpression = (key: string, value: any) => {
+      const newKey = `#${key}`;
+      const newAttrValue = `:${key}`;
+
+      filterExpressions.push(`${newKey} = ${newAttrValue}`);
+      params.ExpressionAttributeValues![newAttrValue] = value;
+      params.ExpressionAttributeNames![newKey] = key;
+    };
+
+    if (status) addFilterExpression("status", status);
+
+    if (rating) addFilterExpression("rating", rating);
+
+    // TODO: Add search to params
+    // if (search) {
+    //   const searchWords = search.split(/\s+/);
+    //   if (searchWords.length > 0) {
+    //     searchWords.forEach((word: string, index: number) => {
+    //       const searchExpression = `contains(#search, :search${index})`;
+    //       filterExpressions.push(searchExpression);
+    //       params.ExpressionAttributeNames!["#search"] = "search";
+    //       params.ExpressionAttributeValues![`:search${index}`] = word;
+    //     });
+    //   }
+    // }
+
+    if (filterExpressions.length > 0) {
+      params.FilterExpression = filterExpressions.join(" AND ");
+    }
+
+    if (!Object.keys(params.ExpressionAttributeNames!).length) {
+      params.ExpressionAttributeNames = undefined;
+    }
+
+    if (!Object.keys(params.ExpressionAttributeValues!).length) {
+      params.ExpressionAttributeValues = undefined;
+    }
+
+    return params;
+  }
+
   async update(reviewId: string, updateDto: Omit<UpdateClinicReviewDto, "userId">) {
     const { updateExpression, expressionAttributeNames, expressionAttributeValues } = updateDataHelper(updateDto);
 
@@ -72,25 +184,6 @@ class ClinicReviewRepository {
     const { Item } = await dynamoClient.send(new GetCommand(params));
 
     return Item ? new ClinicReviewEntity(Item) : null;
-  }
-
-  async findOneByQuery(reviewId: string, userId: string, ProjectionExpression?: string) {
-    const params: QueryCommandInput = {
-      TableName,
-      IndexName: "ReviewUserIndex",
-      KeyConditionExpression: "reviewId = :reviewId AND userId = :userId",
-      ExpressionAttributeValues: {
-        ":reviewId": reviewId,
-        ":userId": userId,
-      },
-      ...(ProjectionExpression && { ProjectionExpression }),
-    };
-
-    const { Items } = await dynamoClient.send(new QueryCommand(params));
-
-    if (Items && Items.length > 0) return new ClinicReviewEntity(Items[0]);
-
-    return null;
   }
 
   async findAllByClinic(clinicId: string, ProjectionExpression?: string) {
--- src/api/clinic-review/clinic-review.router.ts
+++ src/api/clinic-review/clinic-review.router.ts
@@ -2,11 +2,11 @@ import { validateRequest } from "@/common/utils/httpHandlers";
 import { Router } from "express";
 
 import { clinicReviewController } from "./clinic-review.controller";
-import { QueryClinicReviewSchema, QueryOneClinicReviewSchema } from "./dto/query-clinic-review.dto";
+import { FindAllClinicReviewSchema } from "./dto/get-all-clinic-review.dto";
 import { UpdateClinicReviewSchema } from "./dto/update-clinic-review.dto";
 
 export const clinicReviewRouter: Router = Router();
 
+clinicReviewRouter.get("/", validateRequest({ query: FindAllClinicReviewSchema }), clinicReviewController.findAll);
 clinicReviewRouter.patch("/:id", validateRequest({ body: UpdateClinicReviewSchema }), clinicReviewController.update);
-clinicReviewRouter.get("/", validateRequest({ body: QueryClinicReviewSchema }), clinicReviewController.findAllByClinic);
-clinicReviewRouter.get("/:id", validateRequest({ body: QueryOneClinicReviewSchema }), clinicReviewController.findOne);
+clinicReviewRouter.get("/:id", clinicReviewController.findOne);
